3b6d9ffbf82daad0a951a2f4f4e427730bbf5f2f
zcg <zcg@woogiu.com>
2023-07-21 13:57:42 +0800

    1

--- game/ServiceWorker.js
+++ game/ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "DefaultCompany-webgame-1.1";
+const cacheName = "DefaultCompany-webgame-1.2";
 const contentToCache = [
     "Build/game.loader.js",
     "Build/game.framework.js",
